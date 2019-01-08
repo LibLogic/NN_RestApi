1f6bd508ea241bd9b6f731dac0086ba7c23184ad
LibLogic <hdgknsn@gmail.com>
2019-01-08 17:16:54 +0000

    added mongoose, set up connection to cloud based database

--- NN_RestApi/index.js
+++ NN_RestApi/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+app.use(bodyParser.json());
+// connect to mongodb
+const url = 'mongodb://hdgknsn:lino7196@ds151864.mlab.com:51864/ninjas';
+
+mongoose.connect(url, { useNewUrlParser: true } );
+mongoose.Promise = global.Promise;
 
 // use body-parser middleware
 app.use(bodyParser.json());
@@ -10,7 +18,6 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', require('./routes/api'));
 
-// listen for requests
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log('now listening for requests');
+    console.log('okay');
 });
--- RestApi/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-// set up express app
-const app = express();
-
-// listen for requests
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log('now listening for requests');
-});
