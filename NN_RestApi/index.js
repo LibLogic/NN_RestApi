75bfd29dda71881519a860906cc10bd6c4e56bad
LibLogic <hdgknsn@gmail.com>
2019-01-08 19:36:34 +0000

    added error handling

--- NN_RestApi/index.js
+++ NN_RestApi/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 // connect to mongodb
 const url = 'mongodb://hdgknsn:lino7196@ds151864.mlab.com:51864/ninjas';
 
-mongoose.connect(url, { useNewUrlParser: true } );
+mongoose.connect(url, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 // use body-parser middleware
@@ -17,6 +17,12 @@ app.use(bodyParser.json());
 
 // initialize routes
 app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('okay');
